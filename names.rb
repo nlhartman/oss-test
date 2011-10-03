75e68afc210a04c421edbb2e502fd4fbb24f8676
japplebaum <julianapplebaum@gmail.com>
2011-10-02 21:04:08 -0400

    Awesome new feature

--- names.rb
+++ names.rb
@@ -5,3 +5,9 @@ names = ["Jeff",
 ]
 
 names.each {|n| puts "#{n} " + ( rand() > 0.5 ? "rocks!" : "doesn't rock as much :(" )} 
+
+name_1 = names[rand(names.length-1)]
+names.delete(name_1)
+name_2 = names[rand(names.length-1)]
+
+puts "#{name_1}'s name is #{rand(20)} times as common as #{name_2}'s."
